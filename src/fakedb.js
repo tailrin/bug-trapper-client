5aa7f5a955c2f916ef1ae77000004f74fb451e5c
joshua hahn <tailrin.jh@gmail.com>
2020-03-18 19:48:36 -0600

    added fake db

---
+++ src/fakedb.js
@@ -0,0 +1,70 @@
+export default fakeDB = {
+    issues: [
+        {
+            id: 1,
+            description: "test",
+            dateModified: "today",
+            assignedTo: "user1"
+        },
+        {
+            id: 2,
+            description: "test",
+            dateModified: "today",
+            assignedTo: "user1"
+        },
+        {
+            id: 3,
+            description: "test",
+            dateModified: "today",
+            assignedTo: "user1"
+        },
+        {
+            id: 4,
+            description: "test",
+            dateModified: "today",
+            assignedTo: "user1"
+        },
+    ],
+
+    users: [
+        {
+            id: 1,
+            name: "test",
+            role: "standard"
+        },
+        {
+            id: 2,
+            name: "test",
+            role: "standard"
+        },
+        {
+            id: 3,
+            name: "test",
+            role: "standard"
+        },
+    ],
+
+    projects: [
+        {
+            id: 1,
+            name: "test",
+            dateCreated: "today"
+        },
+        {
+            id: 2,
+            name: "test",
+            dateCreated: "today"
+        },
+        {
+            id: 3,
+            name: "test",
+            dateCreated: "today"
+        },
+        {
+            id: 4,
+            name: "test",
+            dateCreated: "today"
+        },
+       
+    ]
+}
