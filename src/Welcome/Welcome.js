7683fa3fc85c8aa56185f1fe80a0a28e3343bdab
joshua hahn <tailrin.jh@gmail.com>
2020-03-27 15:47:26 -0600

    mvp achieved

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -7,7 +7,7 @@ class Welcome extends Component {
 		return (
 			<>
 			<div id="welcome-screen">Welcome to Bug Trapper<br/>
-				<p>This is a basic issue tracker. You must be logged in to use this app. Don't worry I won't collect any of your data. If you want to take a look first before signing up I understand and so have provided a dummy account. The credentials for that dummy account are on the login page.</p>
+				<p id="greeting">This is a basic issue tracker. You must be logged in to use this app. Don't worry I won't collect any of your data. If you want to take a look first before signing up I understand and so have provided a dummy account. The credentials for that dummy account are on the login page.</p>
 			</div>
 			
 			</>
